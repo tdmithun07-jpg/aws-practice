1cc7f6fd5b3a79d8d0fe3a9211dbfdd7d8c08464
tdmithun07-jpg <tdmithun.07@gmail.com>
2026-03-13 17:24:35 +0530

    commit 11

--- modules/networking/main.tf
+++ modules/networking/main.tf
@@ -1,7 +1,17 @@
 resource "aws_vpc" "VPC" {
     cidr_block = "10.0.0.0/16"
   tags = {
-    Name = "my-vpc"
+    Name = var.vpc-name
+  }
+}
+
+resource "aws_subnet" "my_subnet" {
+  vpc_id            =  aws_vpc.VPC.id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = "us-east-2a"
+
+  tags = {
+    Name = "subnet1"
   }
 }
 
