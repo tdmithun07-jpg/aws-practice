9835d48e484280f94df652733ae3323f93fd08de
tdmithun07-jpg <tdmithun.07@gmail.com>
2026-05-30 18:05:05 +0530

    3005 deploy network interface 2

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ module "vpc" {
 
 module "compute" {
   source = "github.com/tdmithun07-jpg/aws-practice/modules/compute"
-  vpc_id = module.vpc.vpc-id
+  vpc-id = module.vpc.vpc-id
   web-subnet-id = module.vpc.web-subnet-id 
   app-subnet-id = module.vpc.app-subnet-id
   db-subnet-id = module.vpc.db-subnet-id
