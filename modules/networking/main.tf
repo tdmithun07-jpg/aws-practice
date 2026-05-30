e11ab8a61721974c8b289c713168c5d09f385170
tdmithun07-jpg <tdmithun.07@gmail.com>
2026-05-30 16:25:28 +0530

    3005 relaunch vpc 1

--- modules/networking/main.tf
+++ modules/networking/main.tf
@@ -5,13 +5,32 @@ resource "aws_vpc" "VPC" {
   }
 }
 
-resource "aws_subnet" "my_subnet" {
+resource "aws_subnet" "web-subnet" {
   vpc_id            =  aws_vpc.VPC.id
-  cidr_block        = "10.0.1.0/24"
+  cidr_block        = var.web-subnet-cidr
   availability_zone = "us-east-2a"
 
   tags = {
-    Name = "subnet-1"
+    Name = var.web-subnet
   }
 }
 
+resource "aws_subnet" "app-subnet" {
+  vpc_id            =  aws_vpc.VPC.id
+  cidr_block        = var.app-subnet-cidr
+  availability_zone = "us-east-2b"
+
+  tags = {
+    Name = var.app-subnet
+  }
+}
+
+resource "aws_subnet" "db-subnet" {
+  vpc_id            =  aws_vpc.VPC.id
+  cidr_block        = var.db-subnet-cidr
+  availability_zone = "us-east-2c"
+
+  tags = {
+    Name = var.db-subnet
+  }
+}
--- modules/networking/variables.tf
+++ modules/networking/variables.tf
@@ -1,3 +1,31 @@
 variable "vpc-name" {
   default = "my-vpc"
 }
+
+variable "vpc-cidr" {
+  default = "10.0.0.0/16"
+}
+
+variable "web-subnet" {
+  default = "web-subnet"
+}
+
+variable "web-subnet-cidr" {
+  default = "10.0.1.0/24"
+}
+
+variable "app-subnet" {
+  default = "app-subnet"
+} 
+
+variable "app-subnet-cidr" {
+  default = "10.0.2.0/24"
+}
+
+variable "db-subnet" {
+  default = "db-subnet"
+}
+
+variable "db-subnet-cidr" {
+  default = "10.0.3.0/24"
+}
