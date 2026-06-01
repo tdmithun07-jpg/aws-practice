918a8dd48b5f6a13646c8da1711339f941698644
tdmithun07-jpg <tdmithun.07@gmail.com>
2026-06-01 12:09:08 +0530

    0106 add sshkey

--- modules/compute/main.tf
+++ modules/compute/main.tf
@@ -1,3 +1,20 @@
+# 1. Look up the secret metadata by its name
+data "aws_secretsmanager_secret" "ssh_key_meta" {
+  name = "sshkey0106"
+}
+
+# 2. Fetch the actual content payload using the secret's ID
+data "aws_secretsmanager_secret_version" "ssh_public_key" {
+  secret_id = data.aws_secretsmanager_secret.ssh_key_meta.id
+}
+
+# Register the public key string as an AWS Key Pair
+resource "aws_key_pair" "web_key" {
+  key_name   = "web-admin-key"
+  public_key = data.aws_secretsmanager_secret_version.ssh_public_key.secret_string
+}
+###############################################################
+
 resource "aws_network_interface" "web-interface" {
   subnet_id   = var.web-subnet-id
 
@@ -7,7 +24,7 @@ resource "aws_network_interface" "web-interface" {
 }
 
 # resource "aws_instance" "web-server" {
-#   ami           = "ami-005e54dee72cc1d00" # us-west-2
+#   ami           = "ami-091138d0f0d41ff90" # us-west-2
 #   instance_type = "t2.micro"
 #   associate_public_ip_address = true # Forces a dynamic public IP
 
@@ -29,7 +46,7 @@ resource "aws_network_interface" "app-interface" {
 }
 
 # resource "aws_instance" "app-server" {
-#   ami           = "ami-005e54dee72cc1d00" # us-west-2
+#   ami           = "ami-091138d0f0d41ff90" # us-west-2
 #   instance_type = "t2.micro"
 #   associate_public_ip_address = true # Forces a dynamic public IP
 #   primary_network_interface {
@@ -50,7 +67,7 @@ resource "aws_network_interface" "db-interface" {
 }
 
 # resource "aws_instance" "db-server" {
-#   ami           = "ami-005e54dee72cc1d00" # us-west-2
+#   ami           = "ami-091138d0f0d41ff90" # us-west-2
 #   instance_type = "t2.micro"
 #   associate_public_ip_address = true # Forces a dynamic public IP
 #   primary_network_interface {
